a4514e7cefa25f861dc64087a862e78d0b70baaa
Zhiqi(Eli) Wang <102864035+ZhiqiEliWang@users.noreply.github.com>
2023-03-21 11:28:27 -0400

    update

--- cuda-reduce.cu
+++ cuda-reduce.cu
@@ -1,6 +1,60 @@
+#include <cooperative_groups.h>
+#include <cooperative_groups/reduce.h>
 #include <stdio.h>
 
 
+namespace cg = cooperative_groups;
+
+// Utility class used to avoid linker errors with extern
+// unsized shared memory arrays with templated type
+template <class T>
+struct SharedMemory {
+  __device__ inline operator T *() {
+    extern __shared__ int __smem[];
+    return (T *)__smem;
+  }
+
+  __device__ inline operator const T *() const {
+    extern __shared__ int __smem[];
+    return (T *)__smem;
+  }
+};
+
+// specialize for double to avoid unaligned memory
+// access compile errors
+template <>
+struct SharedMemory<double> {
+  __device__ inline operator double *() {
+    extern __shared__ double __smem_d[];
+    return (double *)__smem_d;
+  }
+
+  __device__ inline operator const double *() const {
+    extern __shared__ double __smem_d[];
+    return (double *)__smem_d;
+  }
+};
+
+template <class T>
+__device__ __forceinline__ T warpReduceSum(unsigned int mask, T mySum) {
+  for (int offset = warpSize / 2; offset > 0; offset /= 2) {
+    mySum += __shfl_down_sync(mask, mySum, offset);
+  }
+  return mySum;
+}
+
+#if __CUDA_ARCH__ >= 800
+// Specialize warpReduceFunc for int inputs to use __reduce_add_sync intrinsic
+// when on SM 8.0 or higher
+template <>
+__device__ __forceinline__ int warpReduceSum<int>(unsigned int mask,
+                                                  int mySum) {
+  mySum = __reduce_add_sync(mask, mySum);
+  return mySum;
+}
+#endif
+
+// reduce7 is ported from CUDA sample code
 template <typename T, unsigned int blockSize, bool nIsPow2>
 __global__ void reduce7(const T *__restrict__ g_idata, T *__restrict__ g_odata,
                         unsigned int n) {
--- mpi-reduce.c
+++ mpi-reduce.c
@@ -5,8 +5,9 @@
 
 
 
+
 int main(int argc, char* argv[]){
-// ----------------------------------MPI_INIT----------------------------------
+// ----------------------------------local cpu reduce----------------------------------
   // Initialize the MPI environment
     int world_rank, world_size; // init world rank and size
     MPI_Init(&argc, &argv);
@@ -15,51 +16,77 @@ int main(int argc, char* argv[]){
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-// ----------------------------------CUDA_INIT----------------------------------
-    if( (cE = cudaGetDeviceCount( &cudaDeviceCount)) != cudaSuccess )
-    {
-    printf(" Unable to determine cuda device count, error is %d, count is %d\n",
-    cE, cudaDeviceCount );
-    exit(-1);
-    }
-    if( (cE = cudaSetDevice( myrank % cudaDeviceCount )) != cudaSuccess )
-    {
-    printf(" Unable to have rank %d set to cuda device %d, error is %d \n",
-    myrank, (myrank % cudaDeviceCount), cE);
-    exit(-1);
-    }
-
     // size of a array is determined by how many nodes are working on this task
-    int arrSize = (1<<30) / world_size; 
+    int arrSize = (48 * 32**5) / world_size; 
 
-    int* bigArr = malloc(sizeof(int)*arrSize);
+    double* bigArr = malloc(sizeof(int)*arrSize);
 
     for (int i=0; i<arrSize; i++){
-        bigArr[i] = i + world_rank * arrSize;
+        bigArr[i] = (double)i + world_rank * arrSize;
     }
- 
+    uint64_t local_cpu_reduction_start = clock_now();
     // LOCAL SUM
     long long int local_sum = 0; 
     for (int i=0; i<arrSize; i++){
         local_sum += bigArr[i];
     }
+    uint64_t local_cpu_reduction_end = clock_now();
 
+
+
+    // calling MPI_Reduce
+    double global_sum = 0;
+    uint64_t org_start_cycles = clock_now();
+    MPI_Reduce(&local_sum, &global_sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD);
+    uint64_t org_end_cycles = clock_now();
+
+
+    // show runtime
+    if (world_rank == 0){
+        double p2p_time_in_secs = ((double)(p2p_end_cycles - p2p_start_cycles)) / 512000000;
+        printf("MPI Rank %d: Global Sum is %f in %f secs.\n", global_sum, p2p_time_in_secs);
+    }
+
+    free(bigArr);
+// ----------------------------------local cuda reduce----------------------------------
+  // Initialize the MPI environment
+
+    // Find out rank, size
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // size of a array is determined by how many nodes are working on this task
+    arrSize = (1<<30) / world_size; 
+
+    bigArr = malloc(sizeof(int)*arrSize);
+
+    for (int i=0; i<arrSize; i++){
+        bigArr[i] = i + world_rank * arrSize;
+    }
+
+    // LOCAL SUM
+    local_sum = 0; 
+    for (int i=0; i<arrSize; i++){
+        local_sum += bigArr[i];
+    }
 
     // calling MPI_P2P_Reduce
-    long long int global_sum = 0;
-    uint64_t p2p_start_cycles = clock_now();
-    MPI_P2P_Reduce(&local_sum, &global_sum, 1, MPI_LONG_LONG, 0, MPI_COMM_WORLD);
+    global_sum = 0;
+    uint64_t org_start_cycles = clock_now();
+    MPI_Reduce(&local_sum, &global_sum, 1, MPI_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
     MPI_Barrier(MPI_COMM_WORLD);
-    uint64_t p2p_end_cycles = clock_now();
+    uint64_t org_end_cycles = clock_now();
 
+    MPI_Finalize();
 
-    if (world_rank == 0) printf("result from MPI_P2P_Reduce: %lld\n", global_sum);
+    if (world_rank == 0) printf("result from MPI_Reduce:     %lld\n", global_sum);
 
 
         // show runtime
     if (world_rank == 0){
-        double p2p_time_in_secs = ((double)(p2p_end_cycles - p2p_start_cycles)) / 512000000;
-        printf("MPI_P2P_Reduce took %f seconds.\n", p2p_time_in_secs);
+        double org_time_in_secs = ((double)(org_end_cycles - org_start_cycles)) / 512000000;
+        printf("MPI_Reduce took     %f seconds.\n", org_time_in_secs);
     }
 
     free(bigArr);
