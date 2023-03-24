86620d046c87cc6e698f0cc6e3a36e42e03bff2c
Zhiqi(Eli) Wang <102864035+ZhiqiEliWang@users.noreply.github.com>
2023-03-24 08:45:14 -0400

    Update cuda-reduce.cu

--- reduction/gpu_local/cuda-reduce.cu
+++ reduction/gpu_local/cuda-reduce.cu
@@ -1,39 +1,4 @@
-#include <cooperative_groups.h>
-#include <cooperative_groups/reduce.h>
 #include <stdio.h>
-
-
-namespace cg = cooperative_groups;
-
-// Utility class used to avoid linker errors with extern
-// unsized shared memory arrays with templated type
-template <class T>
-struct SharedMemory {
-  __device__ inline operator T *() {
-    extern __shared__ int __smem[];
-    return (T *)__smem;
-  }
-
-  __device__ inline operator const T *() const {
-    extern __shared__ int __smem[];
-    return (T *)__smem;
-  }
-};
-
-// specialize for double to avoid unaligned memory
-// access compile errors
-template <>
-struct SharedMemory<double> {
-  __device__ inline operator double *() {
-    extern __shared__ double __smem_d[];
-    return (double *)__smem_d;
-  }
-
-  __device__ inline operator const double *() const {
-    extern __shared__ double __smem_d[];
-    return (double *)__smem_d;
-  }
-};
 
 template <class T>
 __device__ __forceinline__ T warpReduceSum(unsigned int mask, T mySum) {
